766ffbd7888b332ff0ecea95b115aba6f67d820d
Joelfm4 <48136794+Joelfm4@users.noreply.github.com>
2024-09-05 00:52:13 +0100

    NeoTree

--- init.lua
+++ init.lua
@@ -19,10 +19,10 @@ vim.opt.rtp:prepend(lazypath)
 -- Plugins and Opts
 
 local plugins = {
-	{
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-	}
+	{'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+	{"nvim-neo-tree/neo-tree.nvim", branch = "v3.x", dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim",}},
+
 }
 
 local opts = {}
@@ -33,10 +33,23 @@ require("lazy").setup(plugins, opts)
 
 vim.g.mapleader = " "
 
+
+
 -- Telescope
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 
+
+
+-- Treesitter
+local config = require("nvim-treesitter.configs")
+config.setup({
+	ensure_installed = {"lua", "python", "c", "cpp", "javascript", "cmake", "html", "css"},
+  highlight = { enable = true },
+  indent = { enable = true }, 
+})
+
+vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal right<CR>', {})
 
 
 
