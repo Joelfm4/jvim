624ee434d6bf3b2e2b55cb6c6a2087ffe81f0478
Joelfm4 <48136794+Joelfm4@users.noreply.github.com>
2024-09-06 16:30:52 +0100

    Linter and formatter for javascript and python

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,11 +1,27 @@
 return {
 	"nvimtools/none-ls.nvim",
 
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+
 	config = function()
 		local null_ls = require("null-ls")
+
 		null_ls.setup({
 			sources = {
+				-- Lua
 				null_ls.builtins.formatting.stylua,
+
+				-- JavaScript
+				null_ls.builtins.formatting.prettier,
+				require("none-ls.diagnostics.eslint_d"),
+
+				-- Python
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+
+				-- Cpp
 			},
 		})
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,9 +6,9 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = {"lua", "python", "c", "cpp", "javascript", "cmake", "html", "css"},
+			auto_install = true,
   		highlight = { enable = true },
-  		indent = { enable = true }, 
+  		indent = { enable = true },
 		})
 
 	end
