e000ef07e8f911dbeea69d74f08c3ff4a25d2281
Joelfm4 <48136794+Joelfm4@users.noreply.github.com>
2024-09-07 19:10:34 +0100

    Terminal

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,20 @@
+return {
+
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			direction = "horizontal",
+			open_mapping = "<Leader>ft",
+			shade_filetypes = {},
+			shade_terminals = true,
+			shading_factor = 2,
+			start_in_insert = true,
+			insert_mappings = true,
+			terminal_mappings = true,
+			persist_mode = true,
+			close_on_exit = true,
+			shell = vim.o.shell,
+		})
+	end,
+}
