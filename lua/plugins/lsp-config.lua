fb731ebe71663791af15e91c7a1b8ff05711e0c9
Joelfm4 <48136794+Joelfm4@users.noreply.github.com>
2024-09-09 18:00:04 +0100

    htmldjango compatibility

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,67 +1,77 @@
-
-
 return {
 
 	{
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
-		end
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {"lua_ls", "clangd", "cmake", "cssls", "docker_compose_language_service", "html", "ast_grep", "jsonls", "grammarly", "pyright"}
+				ensure_installed = {
+					"lua_ls",
+					"clangd",
+					"cmake",
+					"cssls",
+					"docker_compose_language_service",
+					"html",
+					"ast_grep",
+					"jsonls",
+					"grammarly",
+					"pyright",
+					"tailwindcss",
+				},
 			})
-		end
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 
 			-- Install language server
 			lspconfig.lua_ls.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
 			lspconfig.clangd.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
 			lspconfig.cmake.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
 			lspconfig.cssls.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
 			lspconfig.docker_compose_language_service.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
 			lspconfig.html.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
+				filetypes = { "html", "htmldjango" },
 			})
 			lspconfig.ast_grep.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
 			lspconfig.jsonls.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
 			lspconfig.grammarly.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
 			})
 			lspconfig.pyright.setup({
-				capabilities = capabilities
+				capabilities = capabilities,
+			})
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
 			})
 
-
 			-- Keymaps
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-			vim.keymap.set('n', 'L', vim.lsp.buf.definition, {})
-			vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-
-
-		end
-	}
-
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "L", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
