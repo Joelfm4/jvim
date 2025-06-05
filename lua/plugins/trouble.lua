04ddd4d09fb2b271fd0242375481aae7c7a91bfc
Joelfm4 <joelfmarques1@gmail.com>
2025-06-05 18:13:54 +0100

    (add) trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,15 @@
+return {
+	"folke/trouble.nvim",
+
+	opts = {},
+  cmd = "Trouble",
+
+	  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics - Trouble",
+    },
+	}
+
+}
