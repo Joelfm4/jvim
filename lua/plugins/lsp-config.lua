dce6339d79af169a98d5f5cb2d2df9089eb94a1f
Joelfm4 <joelfmarques1@gmail.com>
2025-07-08 17:21:20 +0100

    fix syntax

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -31,9 +31,9 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-            local capabilities = require("cmp_nvim_lsp").defaultcapabilities()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
-            for , lsp in ipairs(servers) do
+            for _, lsp in ipairs(servers) do
                 lspconfig[lsp].setup({
                     capabilities = capabilities,
                 })
