a1a763917d32156ebc8bf820200909263076fe4a
joelfm4 <joelfmarques1@gmail.com>
2025-09-18 22:35:20 +0100

    (edit) rebind 'Go to definition'

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -40,7 +40,7 @@ return {
             end
             -- Keymaps
             vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Lsp - Show hover information"})
-            vim.keymap.set("n", "L", vim.lsp.buf.definition, { desc = "Lsp - Go to definition" })
+            vim.keymap.set("n", "D", vim.lsp.buf.definition, { desc = "Lsp - Go to definition" })
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Lsp - Show code actions" })
             vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Lsp - Rename" })
             vim.keymap.set("n", "<leader>ky", "<cmd>Telescope keymaps<CR>", { desc = "Show keymaps" })
