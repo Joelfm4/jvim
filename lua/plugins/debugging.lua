782cb8e0b5dc8732586ed20619c141b94d624b22
joelfm4 <joelfmarques1@gmail.com>
2025-12-28 23:21:09 +0000

    reorganize debugging keymaps

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -24,7 +24,6 @@ return {
       return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") .. ext
     end
 
-    -- Dap Virtual Text
     dap_virtual_text.setup()
 
     -- Mason DAP setup
@@ -91,15 +90,20 @@ return {
 
     -- Keymaps
     vim.keymap.set("n", "<Leader>dc", function() require("dap").continue() end, { desc = "Debugger - Continue debugger" })
+    vim.keymap.set("n", "<Leader>dd", function() require("dap").disconnect({terminateDebuggee = true}) end, { desc = "Debugger - Disconnect debugger" })
+
+
     vim.keymap.set("n", "<Leader>db", function() require("dap").toggle_breakpoint() end, { desc = "Debugger - Toggle breakpoint" })
     vim.keymap.set("n", "<Leader>dB", function() require("dap").set_breakpoint() end, { desc = "Debugger - Set breakpoint" })
     vim.keymap.set("n", "<Leader>dbl", function() require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: ")) end, { desc = "Debugger - Set breakpoint with log point" })
-    vim.keymap.set({ "n", "v" }, "<Leader>di", function() require("dap.ui.widgets").hover() end, { desc = "Debugger - Hover to show variable information" })
-    vim.keymap.set({ "n", "v" }, "<Leader>dii", function() require("dap.ui.widgets").preview() end, { desc = "Debugger - Preview variable information" })
-    vim.keymap.set("n", "<Leader>dso", function() require("dap").step_over() end, { desc = "Debugger - Step over" })
+
+    vim.keymap.set({ "n", "v" }, "<Leader>dvi", function() require("dap.ui.widgets").hover() end, { desc = "Debugger - Hover to show variable information" })
+    vim.keymap.set({ "n", "v" }, "<Leader>dvip", function() require("dap.ui.widgets").preview() end, { desc = "Debugger - Preview variable information" })
+
     vim.keymap.set("n", "<Leader>dsi", function() require("dap").step_into() end, { desc = "Debugger - Step into" })
-    vim.keymap.set("n", "<Leader>dsot", function() require("dap").step_out() end, { desc = "Debugger - Step out" })
-    vim.keymap.set("n", "<Leader>dss", function() local widgets = require("dap.ui.widgets") widgets.centered_float(widgets.frames) end, { desc = "Debugger - Show frames in centered float" })
-    vim.keymap.set("n", "<Leader>dsv", function() local widgets = require("dap.ui.widgets") widgets.centered_float(widgets.scopes) end, { desc = "Debugger - Show scopes in centered float" })
+    vim.keymap.set("n", "<Leader>dso", function() require("dap").step_over() end, { desc = "Debugger - Step over" })
+    vim.keymap.set("n", "<Leader>dsO", function() require("dap").step_out() end, { desc = "Debugger - Step out" })
+
+
   end,
 }
