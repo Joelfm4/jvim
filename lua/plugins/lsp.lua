9b992e684d099ec7f81fbdb53c0bcb0c6b1b6c03
Joelfm4 <joelfmarques1@gmail.com>
2026-02-11 18:48:17 +0000

    Function references

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,8 +58,10 @@ return {
                     end, { desc = "Lsp - Go to definition" })
 
 					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Lsp - Show code actions" })
+                    vim.keymap.set("n", "<leader>ky", "<cmd>Telescope keymaps<CR>", { desc = "Show keymaps" })
+                    vim.keymap.set("n", "<leader>fr", vim.lsp.buf.references, { desc = "Find references" })
 					vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Lsp - Rename" })
-					vim.keymap.set("n", "<leader>ky", "<cmd>Telescope keymaps<CR>", { desc = "Show keymaps" })
+
 			end,
     },
 
